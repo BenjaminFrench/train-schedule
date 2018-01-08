ed864bd570cb7690683547fe1e933888cd06a40a
Ben French <ben.jminw@gmail.com>
2018-01-07 17:24:19 -0700

    Added Firebase init

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBjJYbxHElru4s8Qfxd451Clcar9PnItkc",
+    authDomain: "traintracker-cd7f8.firebaseapp.com",
+    databaseURL: "https://traintracker-cd7f8.firebaseio.com",
+    projectId: "traintracker-cd7f8",
+    storageBucket: "traintracker-cd7f8.appspot.com",
+    messagingSenderId: "477106688938"
+};
+firebase.initializeApp(config);
