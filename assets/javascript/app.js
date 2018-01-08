2f622dcb7b05153dbb675dc8241f919a53d9655c
Ben French <ben.jminw@gmail.com>
2018-01-07 19:37:16 -0700

    Added remove train button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,7 +23,10 @@ var trainFirstTime = "";
 ref.on("child_added", function (snapshot) {
     // Log everything that's coming out of snapshot
     console.log(snapshot.val());
-    // Change the HTML to reflect
+
+    // save the key for the child so we can add button that can remove it
+    let childKey = snapshot.key;
+
     trainName = snapshot.val().trainName;
     trainDestination = snapshot.val().trainDestination;
     trainFrequency = snapshot.val().trainFrequency;
@@ -37,17 +40,30 @@ ref.on("child_added", function (snapshot) {
     let trainMinutesAway = "5";
 
     let print = `
-          <tr>
+           <tr id="train-${trainName}">
                 <td scope="row">${trainName}</td>
                 <td>${trainDestination}</td>
                 <td>${trainFrequency}</td>
                 <td>${trainNextArrival}</td>
                 <td>${trainMinutesAway}</td>
+                <td><button id="${childKey}" class="btn btn-primary" class="remove-train-btn">Delete</button></td>
             </tr>`;
     $('#train-table-body').append(print);
+
+    // Event handler for train deletion
+    $('#'+childKey).on("click", function() {
+        ref.child($(this).attr("id")).remove();
+    });
+
     // Handle the errors
 }, function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
+});
+
+// this fires when a train is removed, i.e., user clicked delete button
+ref.on("child_removed", function (snapshot) {
+    let trainName = snapshot.val().trainName;
+    $("#train-"+trainName).remove();
 });
 
 $('#add-train-btn').on('click', function (event) {
